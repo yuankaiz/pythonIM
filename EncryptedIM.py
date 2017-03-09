60c1d0c1c297c86d6835838ab011828c0223e698
Yuankai Zhang <yuankai@YKMBP.local>
2017-03-09 16:37:55 -0500

    edits

--- EncryptedIM.py
+++ EncryptedIM.py
@@ -82,44 +82,6 @@ def main(args):
         client.run(args.hostname)
 
 
-
-'''
-s = 'helloworld'
-CONFKEY = 'CONFKEY'
-AUTHKEY = 'AUTHKEY'
-
-padded = pad(s) # pad s
-
-hkey = SHA256.new(toBytes(AUTHKEY)).digest()
-
-hmac = HMAC.new(hkey, msg=padded, digestmod=Crypto.Hash.SHA256) # compute hmac
-# print hmac.hexdigest()
-
-h = SHA256.new(toBytes(CONFKEY)) # confkey
-
-
-
-key = h.digest()
-iv = Random.new().read(16)
-
-enc = AES.new(key, AES.MODE_CBC, iv)
-cipher = iv + enc.encrypt(hmac.digest() + padded) # cipher -> iv (16) + enc(hmac(32) + padded)
-
-dec = AES.new(key, AES.MODE_CBC, cipher[:16])
-plain = dec.decrypt(cipher[16:])
-
-computedhmac = HMAC.new(hkey, msg=plain[32:], digestmod=Crypto.Hash.SHA256) # compute hmac
-# print computedhmac.hexdigest()
-
-if hmac.hexdigest() == computedhmac.hexdigest():
-    print 'Authenticated'
-else:
-    print 'Fake'
-
-print len(unpad(plain[32:]))
-print unpad(plain[32:])
-'''
-
 if __name__ == '__main__':
     try:
         import Crypto
